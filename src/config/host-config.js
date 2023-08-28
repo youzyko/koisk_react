cabe9a00ec664ad68aa3866bd2452dabce7b72a1
youzyko <gokongjin@naver.com>
2023-08-28 16:15:41 +0900

    sdfsd

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -7,8 +7,8 @@ console.log('현재호스트: ', hostname);
 let backendHost; // 백엔드 호스트 이름
 if (hostname === 'localhost') {
     backendHost = 'http://localhost:8080';
-} else if (hostname === 'http://ec2-13-124-149-19.ap-northeast-2.compute.amazonaws.com /') {
-    backendHost = 'http://kiosk-db.c39qlc7zi0zc.ap-northeast-2.rds.amazonaws.com/:8080';
+} else if (hostname === 'http://ec2-13-124-149-19.ap-northeast-2.compute.amazonaws.com/') {
+    backendHost = 'http://kiosk-db.c39qlc7zi0zc.ap-northeast-2.rds.amazonaws.com:8080';
 }
 
 export const API_BASE_URL = `${backendHost}`;
