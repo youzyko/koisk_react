06b8ad004d368a490da85c3841a0f3c3eaafcd63
youzyko <gokongjin@naver.com>
2023-08-28 13:19:07 +0900

    ssdf

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -7,8 +7,8 @@ console.log('현재호스트: ', hostname);
 let backendHost; // 백엔드 호스트 이름
 if (hostname === 'localhost') {
     backendHost = 'http://localhost:8080';
-} else if (hostname === '192.168.1.2') {
-    backendHost = 'http://192.168.1.2:8080';
+} else if (hostname === 'http://13.124.149.19/') {
+    backendHost = 'http://13.124.149.19/:8080';
 }
 
 export const API_BASE_URL = `${backendHost}`;
