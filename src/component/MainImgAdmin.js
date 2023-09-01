b1f4fc218e94e9ca63b4cf4d3969f5ce1ac3235a
youzyko <gokongjin@naver.com>
2023-09-01 13:23:04 +0900

    aws10

--- src/component/MainImgAdmin.js
+++ src/component/MainImgAdmin.js
@@ -16,7 +16,8 @@ import { API_BASE_URL } from "config/host-config";
 const MainImgAdmin = () => {
  // const BASE_URL = `${API_BASE_URL}/api/image`;
 //  const BASE_URL = 'http://ec2-13-124-149-19.ap-northeast-2.compute.amazonaws.com:8080/api/image';
-  const BASE_URL = "http://localhost:8080/api/image";
+  // const BASE_URL = "http://localhost:8080/api/image";
+  const BASE_URL = `${API_BASE_URL}/api`;
   const ACCESS_TOKEN = localStorage.getItem("ACCESS_TOKEN");
 
   //id
