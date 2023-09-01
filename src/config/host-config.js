4b73e1feca29bb8052892adbe5227c37097150bd
youzyko <gokongjin@naver.com>
2023-09-01 12:34:26 +0900

    aws7

--- src/config/host-config.js
+++ src/config/host-config.js
@@ -7,8 +7,8 @@ console.log('현재호스트: ', hostname);
 let backendHost; // 백엔드 호스트 이름
 if (hostname === 'localhost') {
     backendHost = 'http://localhost:8080';
-} else if (hostname === 'http://ec2-13-124-149-19.ap-northeast-2.compute.amazonaws.com') {
+} else if (hostname === '13.124.149.19') {
     backendHost = 'http://13.124.149.19:8080';
 }
-console.log(backendHost)
+console.log(backendHost) //undefined 
 export const API_BASE_URL = `${backendHost}`;
